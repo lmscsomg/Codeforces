be8ade9e42cc0c31e48f374ff7944fc65d62bdc1
lms <lmscsomg@gmail.com>
2014-08-16 23:24:50 +0800

    change something

--- 259_Div2/A_Little_Pony_and_Crystal_Mine.cc
+++ 259_Div2/A_Little_Pony_and_Crystal_Mine.cc
@@ -4,18 +4,5 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin>>n;
     
-    for(int i=0;i<n;i++){
-        int num_1=abs(i-(n-1)/2);
-        for(int j=0;j<num_1;j++)
-            cout<<"*";
-        for(int j=0;j<n-2*num_1;j++)
-            cout<<"D";
-        for(int j=0;j<num_1;j++)
-            cout<<"*";
-        cout<<endl;
-    }
-    return 0;
 }
